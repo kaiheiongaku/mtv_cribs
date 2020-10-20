8cce835a3296cb24ac16e3cceab4864518ccd0a5
Robert Heath <robert.b.heathii@gmail.com>
2020-10-20 11:05:46 -0500

    tests for average rent

--- test/building_test.rb
+++ test/building_test.rb
@@ -27,9 +27,25 @@ class BuildingTest < Minitest::Test
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     renter1 = Renter.new("Aurora")
+    renter2 = Renter.new("Tim")
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    assert_equal [], building.renters
 
     unit1.add_renter(renter1)
     assert_equal ["Aurora"], building.renters
+
+    unit2.add_renter(renter2)
+    assert_equal ["Aurora", "Tim"], building.renters
   end
 
+  def test_average_rent
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+    assert_equal 1099.5, building.average_rent
+  end
 end
