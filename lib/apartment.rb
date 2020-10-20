cb0bd43bb57ad49a27ae03d00d61d9497621385c
Robert Heath <robert.b.heathii@gmail.com>
2020-10-20 10:15:45 -0500

    Initialize Apartment

---
+++ lib/apartment.rb
@@ -0,0 +1,14 @@
+class Apartment
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
+
+  def initialize(apartment_information)
+    @number = apartment_information[:number]
+    @monthly_rent = apartment_information[:monthly_rent]
+    @bathrooms = apartment_information[:bathrooms]
+    @bedrooms = apartment_information[:bedrooms]
+  end
+end
