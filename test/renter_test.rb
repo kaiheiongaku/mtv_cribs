04623e0be1eedc52037b54f7f38af54021815941
Robert Heath <robert.b.heathii@gmail.com>
2020-10-20 10:06:33 -0500

    Initial tests for renter

---
+++ test/renter_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+
+class RenterTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    renter1 = Renter.new("Jessie")
+
+    assert_instance_of Renter, renter1
+    assert_equal "Jessie", renter1.name
+  end
+end
